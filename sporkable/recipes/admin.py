f2d26b2155dbd91ec4ee391d864185ae96a67ee3
Michael Gauthier <mikegauthier222@hotmail.com>
2024-08-13 17:14:57 -0400

    fix spacing error

--- sporkable/recipes/admin.py
+++ sporkable/recipes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Recipe
 
-# Register your models here.
+
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'ingredients', 'instructions', 'image_url')
+
+
+admin.site.register(Recipe, RecipeAdmin)
