0e41dfe74085039704ab057e164d0065a6c794c8
Michael Gauthier <mikegauthier222@hotmail.com>
2024-08-13 17:17:15 -0400

    fix spelling error, add recipes app in installed apps

--- sporkable/recipes/models.py
+++ sporkable/recipes/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=255, default="Unknown Title")
+    author = models.CharField(max_length=255, default="Unknown Title")
+    ingredients = models.TextField(default="No ingredients available.")
+    instructions = models.TextField(default="No instructions available.")
+    image_url = models.URLField(default="https://example.com/default_poster.jpg")
+
+    def _str_(self):
+        return self.title
